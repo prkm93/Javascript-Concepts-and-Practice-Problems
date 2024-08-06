20b1018d62c42048c477ab4b664ce7ba26ed1a68
Pradeep Maurya <aradshub93@gmail.com>
2024-08-06 07:47:49 +0530

    another solution

--- problems/10. execute async tasks series/script.js
+++ problems/10. execute async tasks series/script.js
@@ -27,30 +27,52 @@ let tasks = [
   createAsyncTask(),
 ];
 
-asyncSequence(tasks, (error, result) => {
+// asyncSequence(tasks, (error, result) => {
+//   console.log("error", error);
+//   console.log("result", result);
+// });
+
+// // Solution 1
+// function asyncSequence(tasks, callback) {
+//   let results = [];
+//   let errors = [];
+//   let completed = 0;
+
+//   tasks.forEach((promise) => {
+//     promise
+//       .then((res) => {
+//         results.push(res);
+//         completed++;
+//       })
+//       .catch((err) => {
+//         errors.push(err);
+//         completed++;
+//       })
+//       .finally(() => {
+//         if (completed === tasks.length) {
+//           callback(errors, results);
+//         }
+//       });
+//   });
+// }
+
+// Solution 2
+async function asyncSequence2(tasks, callback) {
+  let results = [],
+    errors = [];
+
+  for (let task of tasks) {
+    try {
+      const resp = await task;
+      results.push(resp);
+    } catch (err) {
+      errors.push(err);
+    }
+  }
+  callback(errors, results);
+}
+
+asyncSequence2(tasks, (error, result) => {
   console.log("error", error);
   console.log("result", result);
 });
-
-function asyncSequence(tasks, callback) {
-  let results = [];
-  let errors = [];
-  let completed = 0;
-
-  tasks.forEach((promise) => {
-    promise
-      .then((res) => {
-        results.push(res);
-        completed++;
-      })
-      .catch((err) => {
-        errors.push(err);
-        completed++;
-      })
-      .finally(() => {
-        if (completed === tasks.length) {
-          callback(errors, results);
-        }
-      });
-  });
-}
