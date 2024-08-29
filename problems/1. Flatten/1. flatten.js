7a0778345724039acabfb1ee80e68ac0f0d43850
prkm93 <aradshub93@gmail.com>
2024-08-29 23:41:35 +0530

    flatten.js

--- problems/1. Flatten/1. flatten.js
+++ problems/1. Flatten/1. flatten.js
@@ -12,4 +12,18 @@ const flatten = (arr) => {
 
 const arr = [1,2,3,[4,[5,6]],7,8];
 
+let arr1 = [
+[1,2],
+[3,4],
+[5,6,[7,[20, 21],8],9],
+[10,11,12], 13, 14
+];
+
+/********* Another way ***********/
+
+const flattened = (arr) => {
+  return arr.toString().split(",").map(item => Number(item));
+}
+
 console.log(flatten(arr));
+console.log(flattened(arr1));
